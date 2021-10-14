a84eace64c839ff7d576525c63e3f68379e0e850
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-10-14 15:44:45 +0200

    entry point

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log(`------------------------------------------------------------
+    1. list files on ftp server: $> node list.js
+    2. upload file onto ftp server: $> node upload.js
+    3. download file from ftp server: $> node download.js
+------------------------------------------------------------`);
